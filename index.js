cc20b074dc2953846ab10be668f8aedcd34d9e44
suhailsinghbains <suhail.singhyo@gmail.com>
2019-02-09 22:29:03 +0530

    Updated PORT

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT;
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.get('/', function(req,res){
     res.render("index.html");
 })
-app.listen(3000);
+app.listen(port||3000);
